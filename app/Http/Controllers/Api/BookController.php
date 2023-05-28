885cbfb9d90a6f86a5cdf589077e348b3feef8ff
CiprianBoamba <ciprian.boamba@gmail.com>
2023-05-28 19:18:32 +0100

    update logic put&delete

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -156,38 +156,34 @@ class BookController extends Controller
             'published_year' => 'required|digits:4|before_or_equal:' . now()->year,
         ]);
 
-        if($validator->fails()){
+       
+    if($validator->fails()) {
+        return response()->json([
+            'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+            'errors' => $validator->messages()
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
 
-            return response()->json([
-                'status' => 422,
-                'errors' => $validator->messages()
-            ], 422);
-        } else {
+    try {
+        $book = Book::findOrFail($id);
 
-            $book = Book::find($id);
+        $book->update([
+            'title' => $request->title,
+            'author' => $request->author,
+            'genres' => $request->genres,
+            'published_year' => $request->published_year
+        ]);
 
-        if($book){
-
-
-            
-            $book->update([
-                'title' => $request->title,
-                'author' => $request->author,
-                'genres' => $request->genres,
-                'published_year' => $request->published_year
-            ]);
-
-            return response()->json([
-                'status' => 200,
-                'book' => $book
-            ],200);
-        }else {
-            return response()->json([
-                'status' => 404,
-                'message' => 'No book was found!'
-            ], 404);
-        }
-        }
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'book' => $book
+        ], Response::HTTP_OK);
+    } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+        return response()->json([
+            'status' => Response::HTTP_NOT_FOUND,
+            'message' => 'No book was found!'
+        ], Response::HTTP_NOT_FOUND);
+    }
 
 
     }
@@ -202,20 +198,22 @@ class BookController extends Controller
  */
     public function destroy($id)
     {
-        $book = Book::find($id);
+       try {
+        $book = Book::findOrFail($id);
 
-        if($book){
-            $book->delete($id);
-            return response()->json([
-                'status' => 200,
-                'message' => 'Book Deleted succesfully'
-            ],200);
-        }else{
-            return response()->json([
-                'status' => 404,
-                'message' => 'No book was found!'
-            ], 404);
-        }
+        $book->delete();
+
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'message' => 'Book Deleted successfully!'
+        ], Response::HTTP_OK);
+       } catch (ModelNotFoundException $e) {
+
+        return response()->json([
+            'status' => Response::HTTP_NOT_FOUND,
+            'message' => 'No book was found!'
+        ], Response::HTTP_NOT_FOUND);
+    }
     }
 
 
