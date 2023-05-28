aef81ad914a1ed1476a201e40e0f6e800881f551
CiprianBoamba <ciprian.boamba@gmail.com>
2023-05-28 12:24:31 +0100

    test route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('books', function () {
+    return 'test url: api/books';
+});
